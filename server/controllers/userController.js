c9f5b8e07fa898bc1d49d87802f36a335b38ff36
RavenCrowls <pham.nhathuy4310@gmail.com>
2025-03-15 21:11:50 +0700

    add api for user model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,10 +7,55 @@ const createUser = async (req, res) => {
         await newUser.save();
         res.status(201).json(newUser);
     } catch (error) {
-        res.status(500).json({ message: "Server error", error });
+        res.status(500).json({ message: "Error creating user", error });
     }
 };
 
+const getUser = async (req, res) => {
+    try {
+        const userList = await User.find();
+        res.status(200).json(userList);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting users", error });
+    }
+}
+
+const getDetailUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findOne({ id: userId });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting user", error });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const result = await User.deleteOne({ id: userId });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting user", error });
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const { name, email, avatar, bio, join_date, status, enrollments, } = req.body;
+        const data = { name, email, avatar, bio, join_date, status, enrollments };
+        const updateUser = await User.findOneAndUpdate({ id: userId }, data, { new: true });
+        res.status(200).json(updateUser);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating user", error });
+    }
+}
+
 module.exports = {
     createUser,
+    getUser,
+    getDetailUser,
+    deleteUser,
+    updateUser,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, getUser, getDetailUser, deleteUser, updateUser } = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
+userRouter.get("/", getUser);
+userRouter.get("/:userId", getDetailUser);
+userRouter.delete("/:userId", deleteUser);
+userRouter.put("/:userId", updateUser);
 
 module.exports = { userRouter };
