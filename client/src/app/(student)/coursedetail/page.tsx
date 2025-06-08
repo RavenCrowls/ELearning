81dc318ea5a47882679dc2f89cd113ea4f6d17b6
RavenCrowls <pham.nhathuy4310@gmail.com>
2025-06-08 19:16:06 +0700

    small fix

--- client/src/app/(student)/coursedetail/page.tsx
+++ client/src/app/(student)/coursedetail/page.tsx
@@ -15,7 +15,7 @@ export default function CoursesPage() {
       <nav className='bg-gray-100 py-3 px-4 pl-20 h-[5vh]'>
         <ol className='flex items-center text-sm'>
           <li className='text-gray-500 hover:text-blue-500'>
-            <Link href="/homepage" className='hover:text-blue-600'>Trang chủ</Link>
+            <Link href="/" className='hover:text-blue-600'>Trang chủ</Link>
           </li>
           <li className='mx-2 text-gray-500'>/</li>
           <li className='text-gray-500'>
--- client/src/app/(student)/enrollment/page.tsx
+++ client/src/app/(student)/enrollment/page.tsx
@@ -4,23 +4,23 @@ import Link from 'next/link';
 
 
 
-export default function CoursesPage()  {
+export default function CoursesPage() {
   // Giả lập dữ liệu khóa học
-  return (    
+  return (
     <div className="min-h-screen bg-gray-50">
       <nav className='bg-gray-100 py-3 px-4 pl-20 h-[5vh]'>
-          <ol className='flex items-center text-sm'>
-            <li className='text-gray-500 hover:text-blue-500'>
-              <Link href="/homepage" className='hover:text-blue-600'>Trang chủ</Link>
-            </li>
-            <li className='mx-2 text-gray-500'>/</li>
-            <li className='text-gray-500'>
-              <Link href="/coursefilter" className='text-blue-600 font-semibold'>Khóa học</Link>
-            </li>
-          </ol>
+        <ol className='flex items-center text-sm'>
+          <li className='text-gray-500 hover:text-blue-500'>
+            <Link href="/" className='hover:text-blue-600'>Trang chủ</Link>
+          </li>
+          <li className='mx-2 text-gray-500'>/</li>
+          <li className='text-gray-500'>
+            <Link href="/coursefilter" className='text-blue-600 font-semibold'>Khóa học</Link>
+          </li>
+        </ol>
       </nav>
       <CoursePagewithProgress />
-      
+
     </div>
   );
 };
