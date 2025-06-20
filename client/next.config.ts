71a3261041ae9392d2dd72d9cfa53b353a261b65
RavenCrowls <pham.nhathuy4310@gmail.com>
2025-06-20 20:17:29 +0700

    small fix

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["img.freepik.com", "res.cloudinary.com"],
+    domains: [
+      "img.freepik.com",
+      "res.cloudinary.com",
+      "img.clerk.com",
+      "images.clerk.dev"
+    ],
   },
   devIndicators: false
 };
